58d37b10603025b9ec00c742d0bf4aee5fec9e29
HugoTosi <hugotosi42@gmail.com>
2025-10-17 15:38:47 -0300

    Iniciando projeto

---
+++ gestao/src/main/java/gestao_ti/gestao/GestaoTiApplication.java
@@ -0,0 +1,13 @@
+package gestao_ti.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoTiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoTiApplication.class, args);
+	}
+
+}
---
+++ gestao/src/test/java/gestao_ti/gestao/GestaoTiApplicationTests.java
@@ -0,0 +1,13 @@
+package gestao_ti.gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
