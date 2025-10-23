5ead7994948336a911b3bbad528683406bd0db3e
HugoTosi <hugotosi42@gmail.com>
2025-10-23 09:23:26 -0300

    Removendo lombok de Colaborador e criando Getters, Setters e Construtores

--- gestao/src/main/java/gestao_ti/gestao/entities/Colaborador.java
+++ gestao/src/main/java/gestao_ti/gestao/entities/Colaborador.java
@@ -2,13 +2,8 @@ package gestao_ti.gestao.entities;
 
 import gestao_ti.gestao.enums.EnumStatusColaborador;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
+
 @Entity
 @Table(name = "colaborador_tb")
 public class Colaborador {
@@ -25,4 +20,76 @@ public class Colaborador {
     @Enumerated(EnumType.STRING)
     private EnumStatusColaborador status;
 
+    private String cargo;
+
+    private String setor;
+
+    public Colaborador() {
+    }
+
+    public Colaborador(Long id, String nome, String email, String telefone, EnumStatusColaborador status, String cargo, String setor) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.status = status;
+        this.cargo = cargo;
+        this.setor = setor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public EnumStatusColaborador getStatus() {
+        return status;
+    }
+
+    public void setStatus(EnumStatusColaborador status) {
+        this.status = status;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
 }
