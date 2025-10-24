c9517d568580db9ff5689b5ff73ae1d2cd1bd09e
HugoTosi <hugotosi42@gmail.com>
2025-10-24 08:01:40 -0300

    Funcionalidade para inativar colaboradores

--- gestao/src/main/java/gestao_ti/gestao/repositories/ColaboradorRepository.java
+++ gestao/src/main/java/gestao_ti/gestao/repositories/ColaboradorRepository.java
@@ -3,7 +3,11 @@ package gestao_ti.gestao.repositories;
 import gestao_ti.gestao.entities.Colaborador;
 import gestao_ti.gestao.enums.EnumStatusColaborador;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,4 +17,9 @@ public interface ColaboradorRepository extends JpaRepository<Colaborador, Long> 
     List<Colaborador> findByStatus(EnumStatusColaborador statusColaborador);
 
     List<Colaborador> findByStatusOrderByNomeAsc(EnumStatusColaborador statusColaborador);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Colaborador SET status = :status WHERE id = :id")
+    void atualizarStatus(@Param("id") Long id, @Param("status") EnumStatusColaborador enumStatusColaborador);
 }
